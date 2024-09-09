f6eb3dbe8746c83217e843e949dcfd7069ec1f78
orutanakaito <voltgt777@gmail.com>
2024-09-09 20:42:14 +0900

    "■"のマップを横20と縦20で表示させました。

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+
+// マップ表示する関数
+void drawMap(int rows, int cols, int** p) {
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            std::cout << "■";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main() {
 
+    const int rows = 20;
+    const int cols = 20;
 
-    return 0;
+    // 2次元配列初期化
+    int map[rows][cols]{};
+
+    int** p = new int* [rows];
+
+    // 各行の先頭アドレスを入れる。
+    for (int r = 0; r < rows; r++) p[r] = map[r];
+
+    drawMap(rows, cols, p);
+    delete[]p;
+
 }
