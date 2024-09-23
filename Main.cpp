6a89c48ea4c68bb6d45b9a5e633ea09264a9e336
orutanakaito <voltgt777@gmail.com>
2024-09-23 21:29:23 +0900

    4つのクラスのステータスを表示しました。

--- Main.cpp
+++ Main.cpp
@@ -17,5 +17,20 @@ int main() {
         << "-Strength:" << warr1.getStrength() << '\n'
         << "-Intellect:" << warr1.getIntellect() << '\n';
 
+    std::cout << "Wizard\n"
+        << "-MaxHP:" << wiz1.getMaxHP() << '\n'
+        << "-Strength:" << wiz1.getStrength() << '\n'
+        << "-Intellect:" << wiz1.getIntellect() << '\n';
+
+    std::cout << "Cleric\n"
+        << "-MaxHP:" << cler1.getMaxHP() << '\n'
+        << "-Strength:" << cler1.getStrength() << '\n'
+        << "-Intellect:" << cler1.getIntellect() << '\n';
+
+    std::cout << "Rogue\n"
+        << "-MaxHP:" << rog1.getMaxHP() << '\n'
+        << "-Strength:" << rog1.getStrength() << '\n'
+        << "-Intellect:" << rog1.getIntellect() << '\n';
+
     return 0;
 }
